123ce99b741bba5681ba8b3cdb227bad8f1127a2
root <root@ip-172-31-27-5.us-west-2.compute.internal>
2018-08-30 12:42:36 +0000

    update dummy password after db rebuild:

--- _simple_pythons_examples/show_inedit_db_tables.py
+++ _simple_pythons_examples/show_inedit_db_tables.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  passwd="nikos69fanis!",
+  passwd="nikosfanis!dumpPassforGit",
   database="inedit_db"
 )
 
